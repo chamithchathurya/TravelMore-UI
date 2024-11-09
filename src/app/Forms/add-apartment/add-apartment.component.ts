7d09898ed1b05410d122dc797c1a6f7bddb522f9
chamith chathurya <chamithchathurya00@gmail.com>
2024-11-09 23:54:00 +0530

    connect with backend

--- src/app/Forms/add-apartment/add-apartment.component.ts
+++ src/app/Forms/add-apartment/add-apartment.component.ts
@@ -1,53 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { PropertyListComponent } from '../../property-list/property-list.component';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient,HttpClientModule } from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-apartment',
   standalone: true,
-  imports: [PropertyListComponent,ReactiveFormsModule,HttpClientModule],
+  imports: [FormsModule,CommonModule,HttpClientModule],
   templateUrl: './add-apartment.component.html',
   styleUrl: './add-apartment.component.css'
 })
 export class AddApartmentComponent {
 
-  apartmentForm: FormGroup;
-  selectedFiles: File[]=[];
+  constructor(private http: HttpClient){
+
+  }
+
+  public apartment={
+    name: null,
+    description: null,
+    price:null,
+    location:null,
+    amenities: {
+      wifi: false,
+      parking: false,
+      pool: false,
+      gym: false,
+      ac: false
+    }
+  }
   
-  constructor(private fb:FormBuilder,private http:HttpClient){
-    this.apartmentForm=this.fb.group({
-      apartmentName: [''],
-      description: [''],
-      price: [''],
-      location: [''],
-      amenities: this.fb.group({
-        wifi: [false],
-        parking: [false],
-        pool: [false],
-        gym: [false],
-        ac: [false]
-      }),
-    });
-
-  }
-  onFileChange(event: any) {
-    this.selectedFiles = event.target.files;
-  }
-
-  onSubmit() {
-    const formData = new FormData();
-    Object.keys(this.apartmentForm.value).forEach(key => {
-      formData.append(key, this.apartmentForm.value[key]);
-    });
+  add(){
+    console.log(this.apartment);
+    this.http.post("http://localhost:8080/add-apartment",this.apartment).subscribe(data=>{
+      alert("Apartment Added")
+    })
     
-    // Append files to formData
-    Array.from(this.selectedFiles).forEach(file => {
-      formData.append('images', file);
-    });
-
-    this.http.post('http://localhost:8080/api/apartments', formData)
-      .subscribe(response => console.log(response));
   }
 
+ 
 }
+
+
