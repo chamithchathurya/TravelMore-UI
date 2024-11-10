38a1da7a94f89e5099b06b74df404cef619b135d
chamith chathurya <chamithchathurya00@gmail.com>
2024-11-10 09:28:19 +0530

    add cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { PropertyListComponent } from './property-list/property-list.component';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,PropertyListComponent],
+  imports: [RouterOutlet,HeaderComponent,PropertyListComponent,CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
